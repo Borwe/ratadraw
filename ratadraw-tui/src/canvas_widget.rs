09980a218b4bbf8dd9f85979c751fd0e9d76468b
brian.orwe <brian.orwe@gmail.com>
2025-05-02 14:04:51 +0300

    Fix compile issue

--- ratadraw-tui/src/canvas_widget.rs
+++ ratadraw-tui/src/canvas_widget.rs
@@ -1,11 +1,16 @@
 use std::{collections::HashSet, hash::Hash};
 
-use ratatui::{ crossterm::event::{MouseButton, MouseEvent, MouseEventKind}, layout::{Position, Rect}, symbols, widgets::{Block, Widget}};
+use ratatui::{
+    crossterm::event::{MouseButton, MouseEvent, MouseEventKind},
+    layout::{Position, Rect},
+    symbols,
+    widgets::{Block, Widget},
+};
 
 pub struct MyCell {
     x: u16,
     y: u16,
-    val: &'static str
+    val: &'static str,
 }
 
 impl PartialEq for MyCell {
@@ -22,45 +27,58 @@ impl Hash for MyCell {
     }
 }
 
-impl Eq for MyCell {
-}
+impl Eq for MyCell {}
 
-impl Into<Position> for &MyCell{
+impl Into<Position> for &MyCell {
     fn into(self) -> Position {
-        (self.x,self.y).into()
+        (self.x, self.y).into()
     }
 }
+
+type CellGroup = Vec<MyCell>;
 
 pub struct DrawingCanvas {
     full_region: Option<Rect>,
     actual_region: Option<Rect>,
-    cells: HashSet<MyCell>
+    cells: Vec<CellGroup>,
 }
 
 impl DrawingCanvas {
-    pub(crate) fn new()-> DrawingCanvas{
-        DrawingCanvas { 
+    pub(crate) fn new() -> DrawingCanvas {
+        DrawingCanvas {
             full_region: None,
             actual_region: None,
-            cells: HashSet::default()
+            cells: Vec::default(),
         }
     }
 
     pub(crate) fn listen(&mut self, event: MouseEvent) {
         match event.kind {
             MouseEventKind::Down(x) if x == MouseButton::Left => {
-                self.cells.insert(MyCell { x: event.column, y: event.row, val: symbols::block::FULL });
-            },
-            MouseEventKind::Drag(x) if x == MouseButton::Left => {
-                self.cells.insert(MyCell { x: event.column, y: event.row, val: symbols::block::FULL });
+                let mut new_group = CellGroup::with_capacity(200);
+                new_group.push(MyCell {
+                    x: event.column,
+                    y: event.row,
+                    val: symbols::block::FULL,
+                });
+                self.cells.push(new_group);
             }
-            _ =>{}
+            MouseEventKind::Drag(x) if x == MouseButton::Left => {
+                let vec = self.cells.last_mut().unwrap();
+                vec.push(MyCell {
+                    x: event.column,
+                    y: event.row,
+                    val: symbols::block::FULL,
+                });
+            }
+            _ => {}
         };
     }
 
-    fn generate_rect(&mut self, big_rect: Rect)-> Rect{
+    //get region usable
+    fn generate_rect(&mut self, big_rect: Rect) -> Rect {
         match self.full_region {
-            Some(x)=>{
+            Some(x) => {
                 if x != big_rect {
                     self.update_regions(big_rect)
                 }
@@ -70,13 +88,14 @@ impl DrawingCanvas {
         self.actual_region.unwrap()
     }
 
+    //update region usable
     fn update_regions(&mut self, big_rect: Rect) {
         self.full_region = Some(big_rect);
-        self.actual_region = Some(Rect{
-            x : big_rect.x,
-            y : big_rect.y + 1,
-            width : big_rect.width ,
-            height : big_rect.height -1
+        self.actual_region = Some(Rect {
+            x: big_rect.x,
+            y: big_rect.y + 1,
+            width: big_rect.width,
+            height: big_rect.height - 1,
         })
     }
 }
@@ -84,15 +103,18 @@ impl DrawingCanvas {
 impl Widget for &mut DrawingCanvas {
     fn render(self, area: Rect, buf: &mut ratatui::prelude::Buffer)
     where
-        Self: Sized {
+        Self: Sized,
+    {
         let area = self.generate_rect(area);
         let block = Block::bordered().title("Canvas");
         //let inner_area = block.inner(area);
         block.render(area, buf);
 
-        for c in self.cells.iter() {
-            if let Some(cell) = buf.cell_mut(c){
-                cell.set_symbol(c.val);
+        for cgroup in self.cells.iter() {
+            for c in cgroup.iter() {
+                if let Some(cell) = buf.cell_mut(c) {
+                    cell.set_symbol(c.val);
+                }
             }
         }
     }
--- ratadraw-tui/src/main.rs
+++ ratadraw-tui/src/main.rs
@@ -1,30 +1,40 @@
 use std::{error::Error, io::stdout, time::Duration};
 
 use canvas_widget::DrawingCanvas;
-use ratatui::{crossterm::{event::{self, DisableMouseCapture, EnableMouseCapture, Event, KeyEventKind}, ExecutableCommand}, layout::Rect, prelude::Backend, text::Line, widgets::{Block, Paragraph}, Terminal};
+use ratatui::{
+    crossterm::{
+        event::{self, DisableMouseCapture, EnableMouseCapture, Event, KeyEventKind},
+        ExecutableCommand,
+    },
+    layout::Rect,
+    prelude::Backend,
+    text::Line,
+    widgets::{Block, Paragraph},
+    Terminal,
+};
 use topbar_widget::TopBarWidget;
 
-mod topbar_widget;
 mod canvas_widget;
+mod topbar_widget;
 
 struct App {
     exit: bool,
     topbar: TopBarWidget,
-    canvas: DrawingCanvas
+    canvas: DrawingCanvas,
 }
 
 impl App {
-    fn new(region: Rect)-> Self{
-        Self{
+    fn new() -> Self {
+        Self {
             exit: false,
             topbar: TopBarWidget::default(),
-            canvas: DrawingCanvas::new()
+            canvas: DrawingCanvas::new(),
         }
     }
 
-    fn run<B: Backend>(&mut self, term: &mut Terminal<B>)-> Result<(), Box<dyn Error>>{
+    fn run<B: Backend>(&mut self, term: &mut Terminal<B>) -> Result<(), Box<dyn Error>> {
         while !self.exit {
-            term.draw(|f|{
+            term.draw(|f| {
                 f.render_widget(&self.topbar, f.area());
                 f.render_widget(&mut self.canvas, f.area());
             })?;
@@ -33,26 +43,22 @@ impl App {
         Ok(())
     }
 
-    fn listen(&mut self)-> Result<(),Box<dyn Error>>{
-        match ratatui::crossterm::event::poll(Duration::from_millis(13))?{
-            true => Ok(self.handle_event(event::read()?)) ,
-            false => Ok(())
+    fn listen(&mut self) -> Result<(), Box<dyn Error>> {
+        match ratatui::crossterm::event::poll(Duration::from_millis(13))? {
+            true => Ok(self.handle_event(event::read()?)),
+            false => Ok(()),
         }
     }
 
-    fn handle_event(&mut self, event: Event){
+    fn handle_event(&mut self, event: Event) {
         match event {
-            Event::Key(x) => {
-                match x.kind {
-                    KeyEventKind::Press => {
-                        match x.code {
-                            event::KeyCode::Char('q') => { self.exit = true},
-                            _ => {}
-                        }
-                    },
+            Event::Key(x) => match x.kind {
+                KeyEventKind::Press => match x.code {
+                    event::KeyCode::Char('q') => self.exit = true,
                     _ => {}
-                }
-            }
+                },
+                _ => {}
+            },
             Event::Mouse(x) => self.canvas.listen(x),
             _ => {}
         }
@@ -61,7 +67,7 @@ impl App {
 
 fn main() -> Result<(), Box<dyn Error>> {
     let mut term = ratatui::init();
-    let mut app = App::new(term.get_frame().area());
+    let mut app = App::new();
     stdout().execute(EnableMouseCapture)?;
     app.run(&mut term)?;
     stdout().execute(DisableMouseCapture)?;
