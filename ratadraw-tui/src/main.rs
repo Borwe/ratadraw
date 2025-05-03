bac068c2af797a466cf44eb3b76852bc506ecfaa
brian.orwe <brian.orwe@gmail.com>
2025-05-03 20:55:05 +0300

    Mouse hover done

--- ratadraw-tui/src/main.rs
+++ ratadraw-tui/src/main.rs
@@ -3,13 +3,15 @@ use std::{default, error::Error, io::stdout, time::Duration};
 use canvas_widget::DrawingCanvas;
 use ratatui::{
     crossterm::{
-        event::{self, DisableMouseCapture, EnableMouseCapture, Event, KeyEventKind},
+        event::{
+            self, DisableMouseCapture, EnableMouseCapture, Event, KeyEventKind, MouseEventKind,
+        },
         ExecutableCommand,
     },
-    layout::Rect,
+    layout::{Position, Rect},
     prelude::Backend,
     text::Line,
-    widgets::{Block, Paragraph},
+    widgets::{Block, Paragraph, Widget},
     Terminal,
 };
 use topbar_widget::TopBarWidget;
@@ -20,7 +22,7 @@ mod topbar_widget;
 #[derive(Default)]
 struct App {
     exit: bool,
-    mouse_position: Option<Rect>,
+    mouse_position: Option<Position>,
     topbar: TopBarWidget,
     canvas: DrawingCanvas,
 }
@@ -31,6 +33,7 @@ impl App {
             term.draw(|f| {
                 f.render_widget(&self.topbar, f.area());
                 f.render_widget(&mut self.canvas, f.area());
+                f.render_widget(&self, f.area());
             })?;
             self.topbar.clear_selection();
             self.listen()?
@@ -39,7 +42,7 @@ impl App {
     }
 
     fn listen(&mut self) -> Result<(), Box<dyn Error>> {
-        match ratatui::crossterm::event::poll(Duration::from_millis(13))? {
+        match ratatui::crossterm::event::poll(Duration::from_millis(5))? {
             true => Ok(self.handle_event(event::read()?)),
             false => Ok(()),
         }
@@ -65,8 +68,33 @@ impl App {
                 },
                 _ => {}
             },
-            Event::Mouse(x) => self.canvas.listen(x),
+            Event::Mouse(x) => {
+                self.handle_hover(x);
+                self.canvas.listen(x);
+            }
             _ => {}
+        }
+    }
+
+    fn handle_hover(&mut self, x: event::MouseEvent) {
+        if x.kind == MouseEventKind::Moved {
+            self.mouse_position = Some(Position {
+                x: x.column,
+                y: x.row,
+            });
+        }
+    }
+}
+
+impl Widget for &&mut App {
+    fn render(self, area: Rect, buf: &mut ratatui::prelude::Buffer)
+    where
+        Self: Sized,
+    {
+        if let Some(pos) = self.mouse_position.clone() {
+            if let Some(cell) = buf.cell_mut(pos) {
+                cell.set_bg(ratatui::style::Color::LightGreen);
+            }
         }
     }
 }
